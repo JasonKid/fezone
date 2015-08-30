f80afc2e8aa8831709f68cd308ef515b8c67107c
Jason Peng <jasonpeng0322@gmail.com>
2015-08-30 19:34:21 +0800

    修复了一些postMessage的使用错误

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,5 +2,5 @@ console.log(location.host + ': This would be the main JS file.');
 window.name = 'This is window.name data.';
 
 if(window != window.parent) {
-    postMessage('Message from ' + location.host, window.parent);
+    window.parent.postMessage('Message from ' + location.host, '*');
 }
