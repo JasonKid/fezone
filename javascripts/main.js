74f3dbd38ec2e236cf90fb603062540a1af1a7be
Jason Peng <jasonpeng0322@gmail.com>
2015-08-30 19:07:41 +0800

    设置postMessage

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,2 +1,6 @@
-console.log('This would be the main JS file.');
+console.log(location.host + ': This would be the main JS file.');
 window.name = 'This is window.name data.';
+
+if(window != window.parent) {
+    postMessage('Message from ' + location.host, window.parent);
+}
